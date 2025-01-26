5478f57f1951bbd57f27ff8a477deefd41a18e93
Carolina125 <crobenitez125@gmail.com>
2025-01-26 09:21:37 -0300

    Final touches

--- javascript/index.js
+++ javascript/index.js
@@ -29,16 +29,44 @@ function updateCity(event) {
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
-    <div class="city">
-      <div>
-        <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
-      </div>
-      <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
     "A"
   )}</small></div>
+  </div>
+  <a href="#" id="all-cities">All cities</a>
+  `;
+
+  let allCitiesLink = document.querySelector("#all-cities");
+  allCitiesLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    resetCities();
+  });
+}
+
+function resetCities() {
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city" id="LA">
+      <div>
+        <h2>Los Angeles</h2>
+        <div class="date"></div>
+      </div>
+      <div class="time"></div>
+    </div>
+    <div class="city" id="SYD">
+      <div>
+        <h2>Sydney</h2>
+        <div class="date"></div>
+      </div>
+      <div class="time"></div>
     </div>
   `;
+  update_Time();
 }
 
 update_Time();
